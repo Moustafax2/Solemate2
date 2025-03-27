9a4a81b3bc413545345b11b80b332e7699ecc9ab
Moustafa <moustm4@mcmaster.ca>
2025-03-26 20:51:50 -0400

    Initial commit for SoleMateApp

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,13 +16,13 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">SHOOOOOEEEEESSSSSSSSSSSSSS SOLEMATE SOLEMATE SOLEMATE SOLEMATE</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Edit FF <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
